7c23edef2532b5a32c4d4b7fc24de53ca6b3c65f
Ping Liang Tan <tpl2go@gmail.com>
2025-01-18 15:01:52 +0800

    Fix bug when explorer hides file extension
    Use shell.SHParseDisplayName to obtain PIDL of target files instead of obtaining PIDL of all files in the directory

--- src/showinfm/system/windows.py
+++ src/showinfm/system/windows.py
@@ -15,6 +15,7 @@ from showinfm.system.tools import (
     is_uri,
     path_to_file_uri,
 )
+import os
 
 WindowsFileManagerBehavior = {}
 WindowsFileManagerBehavior["doublecmd.exe"] = FileManagerType.dual_panel
@@ -78,23 +79,9 @@ def launch_file_explorer(paths: List[str], verbose: Optional[bool] = False) -> N
     """
 
     folder_contents = directories_and_their_files(paths)
-
     if folder_contents:
-        desktop = shell.SHGetDesktopFolder()
         for folder in folder_contents:
             folder_pidl = shell.SHILCreateFromPath(folder, 0)[0]
-            shell_folder = desktop.BindToObject(
-                folder_pidl,
-                None,  # type: ignore
-                shell.IID_IShellFolder,
-            )
-            win_items = {
-                desktop.GetDisplayNameOf(item, 0): item
-                for item in shell_folder  # type: ignore
-            }
-            to_select = [
-                win_items[file] for file in folder_contents[folder] if file in win_items
-            ]
             if verbose:
                 files = '", "'.join(folder_contents[folder])
                 if files:
@@ -103,4 +90,5 @@ def launch_file_explorer(paths: List[str], verbose: Optional[bool] = False) -> N
                     "Executing Windows shell to open file "
                     f'explorer at "{folder}", selecting {files}'
                 )
+            to_select = [shell.SHParseDisplayName(os.path.join(folder, filename), 0, None)[0] for filename in folder_contents[folder]]
             shell.SHOpenFolderAndSelectItems(folder_pidl, to_select, 0)  # type: ignore
